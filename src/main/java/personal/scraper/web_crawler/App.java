a0c584da3cc8c2f7bcc937b2c98a4ef459d9e560
Rohit Singhal <irohitsinghal@localhost>
2020-01-19 00:35:03 +0530

    Added concurrency to system

--- src/main/java/personal/scraper/web_crawler/App.java
+++ src/main/java/personal/scraper/web_crawler/App.java
@@ -4,11 +4,15 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -16,23 +20,23 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 /**
- * Hello world!
- *
+ * Web crawler to scrape google results and identify top used Javascript
+ * libraries
  */
 public class App {
 
-	private static Map<String, Integer> libraries = new ConcurrentHashMap<String, Integer>();
+	private Map<String, Integer> libraries = new ConcurrentHashMap<String, Integer>();
 
-	static Document fetchPage(URL url) throws IOException {
+	Document fetchPage(URL url) throws IOException {
 		return Jsoup.connect(url.toString()).userAgent(
 				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36")
 				.header("Content-Language", "en-US").get();
 	}
 
-	static void extractJS(Element head) {
-		Elements scripts = head.select("script[src]");
+	void extractJS(Element head) {
+		Element scripts[] = head.select("script[src]").toArray(new Element[] {});
 
-		for (Element script : scripts) {
+		Stream.of(scripts).parallel().forEach(script -> {
 			try {
 				URL url = new URL(script.absUrl("src"));
 				String tempPaths[] = url.getPath().split("/");
@@ -42,40 +46,44 @@ public class App {
 			} catch (MalformedURLException e) {
 				System.out.println("Could not get js file: " + e.getMessage());
 			}
-		}
+		});
 	}
 
-	public static void main(String[] args) {
-
-		Scanner in = new Scanner(System.in);
-		System.out.print("Enter search Term: ");
-		String searchTerm = in.nextLine();
-		in.close();
-
+	void processRequest(String searchTerm) {
 		String google = "https://www.google.com/search?q=%s&num=%s";
 		List<String> searchType = Arrays.asList("Web results", "Twitter results");
+		int noOfResults = 20;
 		String CHARSET = "UTF-8";
 
 		try {
-			URL url = new URL(String.format(google, URLEncoder.encode(searchTerm, CHARSET)));
+			URL url = new URL(String.format(google, URLEncoder.encode(searchTerm, CHARSET), noOfResults));
 
 			System.out.println("Searching " + searchTerm + " on Google at url: " + url);
 			Document doc = fetchPage(url);
 			Elements headers = doc.select("h2");
 
+			List<Element> results = new ArrayList<>();
 			for (Element header : headers) {
 				if (!searchType.contains(header.text()))
 					continue;
 
-				Elements results = header.nextElementSibling().children();
+				results.addAll(Arrays.asList(header.nextElementSibling().children().toArray(new Element[] {})));
+			}
 
-				for (Element result : results) {
-					String childUrl = null;
+			results.parallelStream().forEach(result -> {
+				String childUrl = null;
+				try {
 					childUrl = result.select("a[href]").first().absUrl("href");
 					extractJS(fetchPage(new URL(childUrl)).head());
+				} catch (IOException e) {
+					System.out.println("Could not fetch page: " + childUrl);
 				}
+			});
 
-			}
+			System.out.println("\nTop 5 most used Javascript libraries:");
+			libraries.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+					.map(e -> e.getKey() + " -> " + e.getValue()).collect(Collectors.toList()).subList(0, 5).stream()
+					.forEach(System.out::println);
 
 		} catch (MalformedURLException e) {
 			System.out.println("Not able to create google search url with queries. Aborting!\n" + e.getMessage());
@@ -84,5 +92,15 @@ public class App {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter search Term: ");
+		String searchTerm = in.nextLine();
+		in.close();
+
+		App app = new App();
+		app.processRequest(searchTerm);
 	}
 }
